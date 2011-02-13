3964039d679b41bd150b81b8f7bb8c4f4f83d2d5
Thomas Thoenes <clumsy@xbmc.org>
2011-02-13 20:05:33 +0100

    version bump to 0.1.7

--- default.py
+++ default.py
@@ -3,7 +3,7 @@ import xbmc, xbmcgui, xbmcplugin, urllib2, urllib, re, string, sys, os, tracebac
 __plugin__ =  'EEVblog'
 __author__ = 'Clumsy <clumsy@xbmc.org>'
 __date__ = '02-05-2010'
-__version__ = '0.1.6'
+__version__ = '0.1.7'
 __settings__ = xbmcaddon.Addon(id='plugin.video.eevblog')
 
 # Thanks to some of the other plugin authors, where I borrowed some ideas from !
